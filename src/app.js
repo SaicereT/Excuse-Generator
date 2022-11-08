5d36cb216f371fcf7af3019a42ae05b50db9e551
SaicereT <justintrejos50@yahoo.com>
2022-11-08 15:42:58 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,34 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  document
+    .getElementById("generateExcuse")
+    .addEventListener("click", generateExcuse);
+
+  function generateExcuse() {
+    let excuse = " ";
+    excuse = randomVal(who) + " ";
+    excuse = excuse + randomVal(action) + " ";
+    excuse = excuse + randomVal(what) + " ";
+    excuse = excuse + randomVal(when);
+
+    document.getElementById("Excuse").innerText = excuse;
+    console.log(excuse);
+  }
+
+  function randomVal(array) {
+    let num = Math.floor(Math.random() * (array.length - 1));
+    return array[num];
+  }
 };
